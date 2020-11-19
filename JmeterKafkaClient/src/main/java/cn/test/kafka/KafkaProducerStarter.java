01833104de0a0ece6fe03c34a9375f7781ee1739
lifei <18550169074@163.com>
2020-11-19 15:45:49 +0800

    压测工具类

--- JmeterKafkaClient/src/main/java/cn/test/kafka/KafkaProducerStarter.java
+++ JmeterKafkaClient/src/main/java/cn/test/kafka/KafkaProducerStarter.java
@@ -1,9 +1,16 @@
 package cn.test.kafka;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.*;
 
 /**
  * @Author: lifei
@@ -11,20 +18,35 @@ import org.apache.jmeter.samplers.SampleResult;
  * @Date: 2020/11/19
  */
 public class KafkaProducerStarter extends AbstractJavaSamplerClient {
+    public static KafkaProducer<String, String> producer;
+
+    static {
+        // 配置信息
+        Properties props = new Properties();
+        // kafka服务器地址
+
+        // 测试环境
+        props.put("bootstrap.servers", "172.30.208.108:9092");
+
+        // 沙箱环境
+//         props.put("bootstrap.servers", "172.30.208.65:9092,172.30.208.66:9092,172.30.208.67:9092");
+//        props.put("bootstrap.servers", "172.30.125.52:9092");
+        // 设置数据key和value的序列化处理类
+        props.put("key.serializer", StringSerializer.class);
+        props.put("value.serializer", StringSerializer.class);
+        // 创建生产者实例
+        producer = new KafkaProducer<>(props);
+    }
 
     @Override
-    /**
-     * JMeter界面中展示出此方法所设置的默认参数。
-     * @return
-     */
     public Arguments getDefaultParameters() {
 
         Arguments args = new Arguments();
         args.addArgument("topic", "test");
         args.addArgument("message", "hello");
-
         return args;
     }
+
     @Override
     public SampleResult runTest(JavaSamplerContext arg0) {
         String topic = arg0.getParameter("topic");
@@ -35,27 +57,100 @@ public class KafkaProducerStarter extends AbstractJavaSamplerClient {
         // System.out.println(context.getParameter("rw"));
         results.sampleStart();
         // 1、获取一个集合foo
-        Boolean result = KafkaUtil.getInstance().sendMsgToProcess(topic, message);
-        if (!result) {
+        ResponseResult result = sendMsgToProcess(topic, processJson(message));
+        if (!result.getResult()) {
             results.setSuccessful(false);
             results.setResponseCode("500");
         } else {
             results.setResponseCodeOK();
             results.setSuccessful(true);
         }
+        results.setResponseMessage(Long.toString(result.getTimestamp()));
         results.sampleEnd();
         return results;
     }
 
     public static void main(String[] args) {
-        Arguments a = new Arguments();
-        a.addArgument("topic", "test");
-        a.addArgument("message", "hello");
+        KafkaProducerStarter kafkaProducerStarter = new KafkaProducerStarter();
+        String result = kafkaProducerStarter.processJson("");
+        System.out.println(result);
+    }
 
-        final JavaSamplerContext c = new JavaSamplerContext(a);
-        long startTime = System.currentTimeMillis();
-        new KafkaProducerStarter().runTest(c);
-        long endTime = System.currentTimeMillis();
-        System.out.println(endTime - startTime);
+    public static ResponseResult sendMsgToProcess(String kafkaTopic, String data) {
+        ProducerRecord record = new ProducerRecord<String, String>(kafkaTopic, data);
+        try {
+            producer.send(record).get();
+            ResponseResult responseResult = new ResponseResult();
+            responseResult.setResult(true);
+            responseResult.setTimestamp(System.currentTimeMillis());
+            return responseResult;
+        } catch (Exception e) {
+            e.printStackTrace();
+            ResponseResult responseResult = new ResponseResult();
+            responseResult.setResult(false);
+            responseResult.setTimestamp(System.currentTimeMillis());
+            return responseResult;
+        }
+    }
+
+    private String processJson(String rawData) {
+        JSONObject jsonObject = JSONObject.parseObject(rawData);
+        Set<String> keys = jsonObject.keySet();
+        JSONObject result = null;
+        // 剥离掉第一层
+        for (String key : keys) {
+            Object value = jsonObject.get(key);
+            JSONArray jsonArray = (JSONArray)value;
+            // 数组中只有一个元素
+            JSONObject jsonStr = (JSONObject)jsonArray.get(0);
+            System.out.println(jsonStr);
+            String messageId = UUID.randomUUID().toString().replaceAll("-", "");
+
+            result = modifyMessageId(jsonStr, messageId);
+        }
+        return JSONObject.toJSONString(result);
+    }
+
+    public JSONObject modifyMessageId(JSONObject jsonObject, String messageId) {
+        System.out.println(messageId);
+        Map<String, Object> allKeyValue = getAllKeyValue(jsonObject);
+        for (String key : allKeyValue.keySet()) {
+            if ("message_id".equals(key)) {
+                jsonObject.put("message_id", messageId);
+            }
+            if ("messageId".equals(key)) {
+                jsonObject.put("messageId", messageId);
+            }
+            if ("cmd_data".equals(key) || "rawdata".equals(key) || "event_data".equals(key)) {
+                String body = processBodyMessage((String)allKeyValue.get(key), messageId);
+                jsonObject.put(key, body);
+            }
+        }
+        return jsonObject;
+    }
+
+    private String processBodyMessage(String jsonString, String messageId) {
+        JSONObject jsonObject = JSONObject.parseObject(jsonString);
+        Map<String, Object> allKeyValue = getAllKeyValue(jsonObject);
+        for (String key : allKeyValue.keySet()) {
+            if ("message_id".equals(key)) {
+                jsonObject.put("message_id", messageId);
+            }
+            if ("messageId".equals(key)) {
+                jsonObject.put("messageId", messageId);
+            }
+        }
+        return JSONObject.toJSONString(jsonObject);
+    }
+
+    // 公共属性中存在为数字类型
+    public static Map<String, Object> getAllKeyValue(JSONObject jsonObject) {
+        Set<String> keys = jsonObject.keySet();
+        Map<String, Object> list = new HashMap<>();
+        for (String key : keys) {
+            Object value = jsonObject.get(key);
+            list.put(key, value);
+        }
+        return list;
     }
 }
